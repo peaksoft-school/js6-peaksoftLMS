b171fca86aa75d0bd841f1329adaf7671fbca87c
Elizar <elizar.aitbek@gmail.com>
2022-10-06 17:02:21 +0600

    git conflict solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import { MeadBalls } from './components/UI/MeadBalls'
 
 export const App = () => {
@@ -8,9 +7,4 @@ export const App = () => {
          <MeadBalls />
       </div>
    )
-=======
-
-export const App = () => {
-   return <div>App</div>
->>>>>>> 2633ce5a9fb7ac78105c25f3b2d50673d93c94de
 }
