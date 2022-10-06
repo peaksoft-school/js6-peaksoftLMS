013bd26d0c81f51722ca7adcf29f15217e77e925
Dastanbek <dastanbekrahmanov01@gmail.com>
2022-10-06 19:02:56 +0600

    чистка апп жс

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import TextArea from './components/UI/TextArea'
 
 export const App = () => {
-   return (
-      <div>
-         <TextArea placeholder="Описания группы" name="text" />
-      </div>
-   )
+   return <div>App</div>
 }
