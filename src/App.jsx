1b8f3becebc00c2fc57378c3cb2751502ec892b0
Elizar <elizar.aitbek@gmail.com>
2022-10-06 14:37:37 +0600

    modal windon is ready to use

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { ModalWindow } from './components/UI/ModalWindow'
 
 export const App = () => {
-   return <div>App</div>
+   return (
+      <div>
+         <ModalWindow
+            modalTitle="some long title"
+            headerContent="some header content"
+            bodyContent="some body content"
+            footerContent="some footer content"
+         />
+      </div>
+   )
 }
---
+++ src/components/UI/ModalWindow.jsx
@@ -0,0 +1,56 @@
+import { Button, Grid, Modal, styled, Typography } from '@mui/material/'
+import { useState } from 'react'
+
+export const ModalWindow = ({
+   modalTitle,
+   bodyContent,
+   headerContent,
+   footerContent,
+}) => {
+   const [open, setOpen] = useState(false)
+   const handleOpen = () => setOpen(true)
+   const handleClose = () => setOpen(false)
+   return (
+      <>
+         <Button onClick={handleOpen} variant="contained">
+            Contained
+         </Button>
+         <ModalMain open={open} onClose={handleClose}>
+            <ModalBlock>
+               <TitleBlock>{modalTitle}</TitleBlock>
+               <Grid>{headerContent}</Grid>
+               <Grid>{bodyContent}</Grid>
+               <Grid>{footerContent}</Grid>
+            </ModalBlock>
+         </ModalMain>
+      </>
+   )
+}
+
+const ModalMain = styled(Modal)`
+   height: 100vh;
+   width: 100vw;
+   position: fixed;
+   top: 0;
+   left: 0;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`
+
+const ModalBlock = styled(Grid)`
+   border-radius: 10px;
+   background-color: #fff;
+   text-align: center;
+   padding: 0 0 10px 0;
+   width: 540px;
+`
+const TitleBlock = styled(Typography)`
+   border-radius: 10px 10px 0 0;
+   width: auto;
+   font-size: 20px;
+   font-weight: 400;
+   padding: 25px 190px;
+   background: #1f6ed4;
+   color: #fff;
+`
