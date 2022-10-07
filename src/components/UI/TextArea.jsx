b124dcd747f25c3241fe34607ab429ca4137bbb1
Dastanbek <dastanbekrahmanov01@gmail.com>
2022-10-07 12:50:13 +0600

    resolve commit props

--- src/components/UI/TextArea.jsx
+++ src/components/UI/TextArea.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function TextArea({ placeholder, name }) {
+function TextArea({ ...props }) {
    return (
       <TextAreaContainer>
-         <TextAreaIn placeholder={placeholder} name={name} />
+         <TextAreaIn {...props} />
       </TextAreaContainer>
    )
 }
