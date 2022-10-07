fbdf9803a213bd561aa0647e665a47a9253d93d3
Ramazan <ramazanmahmudov03@gmail.com>
2022-10-07 19:46:27 +0600

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import UiInputPassword from './components/UI/UiInputPassword'
 
 export const App = () => {
-   return (
-      <div>
-         <UiInputPassword />
-      </div>
-   )
+   return <div>App</div>
 }
