6aff8ab41d7842a17527daa904d87d6e9055844f
Dastanbek <dastanbekrahmanov01@gmail.com>
2022-10-07 12:36:07 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import TextArea from './components/UI/TextArea'
 import GroupCard from './components/UI/GroupCard'
 
 function App() {
    return (
       <div>
-         {/* <TextArea /> */}
          <GroupCard
             someImage="https://www.fa-mag.com/userfiles/0000002019_IMAGES_ALL/FA_ISSUES_2019/10_FA_OCTOBER_2019/LONGVIEW_Lessons-From-A-Young-Professional_Patel--600x400.jpg"
             someName="Data Engineer"
