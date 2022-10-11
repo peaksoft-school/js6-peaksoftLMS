0c91c1051d2d51df5b5296879fe87e71fc62f69c
Dastanbek <dastanbekrahmanov01@gmail.com>
2022-10-11 14:59:16 +0600

    some changes datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import DatePicker from './components/UI/DatePicker'
 
 export const App = () => {
-   return <div>App</div>
+   return (
+      <div>
+         <DatePicker />
+      </div>
+   )
 }
