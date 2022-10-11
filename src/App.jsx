383d8926f6775a17c172b40502a1382aa19b53dd
Ramazan <ramazanmahmudov03@gmail.com>
2022-10-11 14:46:39 +0600

    clear App IsPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import UiIsPassword from './components/UI/UiIsPassword'
 
 export const App = () => {
-   return (
-      <div>
-         App
-         <UiIsPassword />
-      </div>
-   )
+   return <div>App</div>
 }
