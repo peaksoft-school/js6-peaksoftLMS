814dd5a2081cf380766c2544970dd09654b4deda
Kuma <kumagithub@mail.ru>
2022-10-13 16:03:12 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import PopUp from './components/UI/PopUp'
 
 export const App = () => {
-   return (
-      <div>
-         <PopUp/>
-      </div>
-   )
+   return <div> </div>
 }
