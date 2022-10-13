94b214080c2120a8bb0aaa939cb1f168be1d3d24
Kuma <kumagithub@mail.ru>
2022-10-14 03:04:39 +0600

    Submit for review

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import LessonCard from './components/UI/LessonCard'
 
 export const App = () => {
-   return (
-      <div>
-         <LessonCard />
-      </div>
-   )
+   return <div>App</div>
 }
---
+++ src/components/UI/ImagePicker.jsx
@@ -0,0 +1,99 @@
+import styled from 'styled-components'
+import { useDropzone } from 'react-dropzone'
+import { useState } from 'react'
+import imagePickerIcon from '../../assets/ImagePickerIcon.svg'
+
+function ImagePicker({ GetImg }) {
+   const [files, setFiles] = useState(null)
+   const onDrop = (acceptedFiles) => {
+      setFiles(
+         acceptedFiles.map((file) =>
+            Object.assign(file, {
+               preview: URL.createObjectURL(file),
+            })
+         )
+      )
+      GetImg(acceptedFiles)
+   }
+   const { getRootProps, getInputProps } = useDropzone({
+      accept: {
+         'image/jpeg': [],
+         'image/png': [],
+         'image/JPG': [],
+      },
+      maxFiles: 1,
+      onDrop,
+   })
+
+   return (
+      <Container>
+         <MainPicker>
+            {files ? (
+               <ImageWrapper>
+                  <img
+                     src={files[0].preview}
+                     style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                     }}
+                     alt="preview"
+                  />
+               </ImageWrapper>
+            ) : (
+               <DropContainer {...getRootProps()}>
+                  <ToAddImg src={imagePickerIcon} alt="" />
+                  <input {...getInputProps()} />
+               </DropContainer>
+            )}
+         </MainPicker>
+         <Text>Нажмите на иконку чтобы загрузить или перетащите фото</Text>
+      </Container>
+   )
+}
+export default ImagePicker
+const Container = styled.div`
+   display: flex;
+   width: 241px;
+   text-align: center;
+   justify-content: center;
+   flex-direction: column;
+   align-items: center;
+`
+const MainPicker = styled.div`
+   background: #f6f6f9;
+   width: 173px;
+   height: 145px;
+   border-radius: 10px;
+   position: relative;
+   cursor: pointer;
+`
+const ImageWrapper = styled.div`
+   width: 100%;
+   height: 100%;
+   display: flex;
+   align-items: center;
+   & > img {
+      border-radius: 8px;
+   }
+`
+const DropContainer = styled.div`
+   position: absolute;
+   width: 100%;
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+`
+const ToAddImg = styled.img``
+const Text = styled.span`
+   margin-top: 6px;
+   font-style: normal;
+   font-weight: 400;
+   font-size: 14px;
+   line-height: 130%;
+   text-align: center;
+   letter-spacing: 0.03em;
+   color: #8d949e;
+`
