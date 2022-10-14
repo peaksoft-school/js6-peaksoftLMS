19dd32a63922a9b6a4a8f8541f121628a2de3fc5
Kuma <kumagithub@mail.ru>
2022-10-14 15:33:19 +0600

    fixed issues

--- src/components/UI/Switcher.jsx
+++ src/components/UI/Switcher.jsx
@@ -2,29 +2,35 @@
 import { FormControlLabel, Switch } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-export const Switcher = () => {
+export const Switcher = ({ setIsClicked }) => {
+   const clickHandler = () => {
+      setIsClicked((prev) => !prev)
+   }
+
    return (
       <FormControlLabel
+         onClick={clickHandler}
          control={<CustomSwitch sx={{ m: 1 }} defaultChecked />}
       />
    )
 }
+
 const CustomSwitch = styled((props) => (
    <Switch size="medium" focusVisibleClassName=".Mui-focusVisible" {...props} />
-))(({ theme }) => ({
-   width: 42,
-   height: 26,
+))(() => ({
+   width: 51,
+   height: 30,
    padding: 0,
+   borderRadius: 100,
    '& .MuiSwitch-switchBase': {
-      padding: 0,
-      margin: 2,
+      padding: 4,
+      margin: 1,
       color: '#C91E1E',
       '&.Mui-checked': {
-         transform: 'translateX(16px)',
+         transform: 'translateX(20px)',
          color: '#36AC0C',
          '& + .MuiSwitch-track': {
-            backgroundColor:
-               theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+            backgroundColor: '#fff',
             opacity: 1,
             border: 0,
          },
@@ -32,7 +38,7 @@ const CustomSwitch = styled((props) => (
    },
    '& .MuiSwitch-track': {
       borderRadius: 26 / 2,
-      backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+      backgroundColor: '#fff',
       opacity: 1,
    },
 }))
