6d4a6b8e74a055e5e7cf3393ea3f7a43038faaa3
Kuma <kumagithub@mail.ru>
2022-10-22 18:47:37 +0600

    finish

---
+++ src/components/UI/Wrapper.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = ({ children, width, height, padding, margin }) => {
+   return (
+      <Container
+         width={width}
+         height={height}
+         padding={padding}
+         margin={margin}
+      >
+         {children}
+      </Container>
+   )
+}
+
+const Container = styled.div`
+   width: ${(props) => props.width};
+   height: ${(props) => props.height};
+   padding: ${(props) => props.padding};
+   margin: ${(props) => props.margin};
+   border-radius: 10px;
+   border: 1px solid #d4d4d4;
+   background-color: #ffff;
+`
+export default Wrapper
