7b9e4c8330578568b47631190a71314236a6ceba
Kuma <kumagithub@mail.ru>
2022-11-14 17:11:06 +0600

    intermediate storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { AppRoutes } from './routes/AppRoutes'
+import Test from './components/UI/Test'
 
 export const App = () => {
    const location = useLocation()
    if (location.pathname === '/') return <Navigate to="/login" replace />
-
-   return <AppRoutes />
+   return (
+      <>
+         <AppRoutes />
+         <Test />
+      </>
+   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authSlice } from './slices/authSlice'
 import { userSlice } from './slices/userSlice'
 
 const store = configureStore({
    reducer: {
       user: userSlice.reducer,
+      auth: authSlice.reducer,
    },
 })
-
 export default store
