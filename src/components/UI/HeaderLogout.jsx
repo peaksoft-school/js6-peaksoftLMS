1327194067e5703e22c26481965d85936b2c29b0
Kuma <kumagithub@mail.ru>
2022-11-25 18:48:58 +0600

    code review

--- src/components/UI/HeaderLogout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const HeaderLogout = () => {
-   return <div>Sasukeee</div>
-}
-
-export default HeaderLogout
--- src/containers/admin/GroupsPage.jsx
+++ src/containers/admin/GroupsPage.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import HeaderLogout from '../../components/UI/HeaderLogout'
 
 export const GroupsPage = () => {
-   return (
-      <>
-         <div>GroupsPage</div> <HeaderLogout />
-         <Outlet />
-      </>
-   )
+   return <Outlet />
 }
